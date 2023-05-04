f89d295e823d5e07c477e399e0207789938eadd6
guschang2013 <77599534+guschang2013@users.noreply.github.com>
2023-05-04 21:29:31 +0000

    add goodbye

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 def print_hello():
     return "Hi"
+
+def print_goodbye():
+    return "bye"
--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,7 @@
-from hello import print_hello
+from hello import print_hello, print_goodbye
 
 def test_print_hello():
     assert "Hi" == print_hello()
+
+def test_print_goodbye():
+    assert "hellO" == print_goodbye()
